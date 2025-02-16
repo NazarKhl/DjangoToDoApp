e1def5b2a18e0fceb2ef7e3b365e34b444712b84
Nazar Khlivnyi <135509585+NazarKhl@users.noreply.github.com>
2025-02-16 14:59:43 +0100

    Working with database connection

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'YOUR_DATABASE',
-        'USER': 'YOUR_ROOT', 
-        'PASSWORD': 'YOUR_PASSWORDD', 
-        'HOST': 'YOUR_HOST',  
-        'PORT': 'YOUR_PORT',  
+         'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
     }
 }
 
